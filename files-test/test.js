41cbf6796af865a1072f585dd58979d8c1a05863
hamza abdo ali <106634375+hamzaabdo304@users.noreply.github.com>
2023-09-30 16:11:08 +0300

    files
    project files

---
+++ files-test/test.js
@@ -0,0 +1,8 @@
+{
+    var n = "5436";
+}
+var name_value = "Valentino";
+var salary = 54.32;
+var hex_value = 0xf00d;
+var binary = 10;
+var octal = 484;
---
+++ files-test/test.ts
@@ -0,0 +1,9 @@
+{
+    let n:string="5436";
+
+}
+let name_value:string ="Valentino";
+let salary:number=54.32;
+let hex_value:number=0xf00d;
+let binary:number=0b1010;
+let octal:number=0o744;
